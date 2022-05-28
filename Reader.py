eb641f9632ced0e3134d281178abd2aab52a3657
nelajovic14 <nelanekijovic@gmail.com>
2022-05-28 17:19:49 +0200

    Uspostavljanje konekcije izmedju Reader komponente i Replicator

--- Reader.py
+++ Reader.py
@@ -3,3 +3,23 @@ import cx_Oracle
 
 con = cx_Oracle.connect('bp1/ftn@localhost')
 
+import socket
+
+serverSocket = socket.socket()
+localHost = "127.0.0.1"
+port = 10003
+
+try:
+    serverSocket.bind((localHost, port))
+except socket.error as e:
+    print(str(e))
+
+print("Waiting for a connection...")
+serverSocket.listen(5)
+
+client, address = serverSocket.accept()
+print("Connect to: " + address[0] + ":" + str(address[1]))
+
+poruka = client.recv(2048)
+poruka=poruka.decode("utf-8")
+print("Server primio poruku od klijenta " + poruka)
