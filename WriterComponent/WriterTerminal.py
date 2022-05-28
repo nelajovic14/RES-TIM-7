2c28c9e7dfd6e6ed68f123964375c37615227de7
KatarinaProdanovic <katarina.prodanovic20@gmail.com>
2022-05-28 17:26:45 +0200

    Uspostavljanje konekcije izmedju Writer i Replicator komponente

--- WriterComponent/WriterTerminal.py
+++ WriterComponent/WriterTerminal.py
@@ -1,4 +1,5 @@
-
+from email import message
+import socket
 from WriterClass import Message
 
 def unosPodataka():
@@ -11,4 +12,29 @@ def unosPodataka():
 
     Message.__init__(message, id, potrosnja)
     return message
+
+
+
+def konekcija():
+    clientSocket = socket.socket()
+    localHost = "127.0.0.1"
+    port = 10001
+
+    print("Waiting for connection")
+
+    try:
+        clientSocket.connect((localHost, port))
+    except socket.error as e:
+        print(str(e))
+
+    message = unosPodataka()
+
+    clientSocket.send(str.encode(Message.__str__(message)))
+    print("Klijent uspesno poslao ID")
+
+
+    clientSocket.close()
+
+
+konekcija()
 
