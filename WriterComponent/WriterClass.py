57eaf23c5e378fb157fd0536cec7c21dec76dcbb
KatarinaProdanovic <katarina.prodanovic20@gmail.com>
2022-06-03 14:14:03 +0200

    Slanje informacija od Writer ka Replicator komponenti

--- WriterComponent/WriterClass.py
+++ WriterComponent/WriterClass.py
@@ -1,6 +1,13 @@
 class Message:
-    def __init__(self, id, potrosnja):
+    def __init__(self, id, potrošnja, mesec, korisnikIme, korisnikPrezime):
         self.id = id
-        self.potrosnja = potrosnja
+        self.potrošnja = potrošnja
+        self.mesec = mesec
+        self.korisnikIme = korisnikIme
+        self.korisnikPrezime = korisnikPrezime
+        
+        
+        
+    
     def __str__(self):
-        return self.id + " " + self.potrosnja
+        return "Server primio podatke od korisnika: "+ self.korisnikIme + " " +  self.korisnikPrezime + "\n" +"id:" + " "+ self.id + "\n" + "potrošnja: "  + ""+ self.potrošnja + "\n" + "mesec: " + self.mesec
