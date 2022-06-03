836f90b44b2900c5ee9b4634c5577f222b7ca4df
nelajovic.14 <nelanekijovic@gmail.com>
2022-06-03 18:19:39 +0200

    Upis podataka u bazu

--- Reader.py
+++ Reader.py
@@ -26,6 +26,7 @@ def konekcija():
     potrosnja=((poruka.split('\n')[2]).split(':')[1]).split(' ')[0]
     mesec=((poruka.split(' ')[3]).split(':')[1]).split(' ')[0]
 
+    upisUBazu(id,potrosnja,mesec)
     print("Server primio poruku od klijenta " + poruka)
 
     serverSocket.close()
@@ -33,5 +34,21 @@ def konekcija():
 def konekcijaKaBazi():
     con = cx_Oracle.connect('bp1/ftn@localhost')
     return con
+
+def IdPotrosnje():
+    con=konekcijaKaBazi()
+    sqlquery="select count(*) from POTROSNJA"
+    cursor = con.cursor()
+    n=cursor.execute(sqlquery)
+    return n
+
+def upisUBazu(id,potrosnja,mesec):
+    con=konekcijaKaBazi()
+    br=IdPotrosnje()
+    br+=1
+    sqlquery="insert into POTROSNJA values ("+br+","+id+","+potrosnja+","+mesec+")"
+    cursor = con.cursor()
+    cursor.execute(sqlquery)
+
 
 konekcija()
