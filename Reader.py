5fa88037e3c8b2d9183cf963d646be5368b26f6b
nelajovic.14 <nelanekijovic@gmail.com>
2022-06-03 17:31:43 +0200

    Prijem poruke od Reader komponente i priprema podataka za upis u bazu

--- Reader.py
+++ Reader.py
@@ -1,25 +1,37 @@
 import logging
 import cx_Oracle
-
-con = cx_Oracle.connect('bp1/ftn@localhost')
-
 import socket
 
-serverSocket = socket.socket()
-localHost = "127.0.0.1"
-port = 10003
+def konekcija():
+    serverSocket = socket.socket()
+    localHost = "127.0.0.1"
+    port = 10003
 
-try:
-    serverSocket.bind((localHost, port))
-except socket.error as e:
-    print(str(e))
+    try:
+        serverSocket.bind((localHost, port))
+    except socket.error as e:
+        print(str(e))
 
-print("Waiting for a connection...")
-serverSocket.listen(5)
+    print("Waiting for a connection...")
+    serverSocket.listen(5)
 
-client, address = serverSocket.accept()
-print("Connect to: " + address[0] + ":" + str(address[1]))
+    client, address = serverSocket.accept()
+    print("Connect to: " + address[0] + ":" + str(address[1]))
 
-poruka = client.recv(2048)
-poruka=poruka.decode("utf-8")
-print("Server primio poruku od klijenta " + poruka)
+    poruka = client.recv(2048)
+    poruka=poruka.decode("utf-8")
+
+
+    id=((poruka.split('\n')[1]).split(':')[1]).split(' ')[0]
+    potrosnja=((poruka.split('\n')[2]).split(':')[1]).split(' ')[0]
+    mesec=((poruka.split(' ')[3]).split(':')[1]).split(' ')[0]
+
+    print("Server primio poruku od klijenta " + poruka)
+
+    serverSocket.close()
+
+def konekcijaKaBazi():
+    con = cx_Oracle.connect('bp1/ftn@localhost')
+    return con
+
+konekcija()
