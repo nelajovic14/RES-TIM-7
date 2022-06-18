31b3038e657855d13c0c4f86790b4b97b3c55339
nelajovic.14 <nelanekijovic@gmail.com>
2022-06-18 22:04:33 +0200

    Zavrsetak testiranja Reader komponente

--- KonekcijaKaBazi.py
+++ KonekcijaKaBazi.py
@@ -1,16 +1,25 @@
+
 from distutils.log import error
 from msilib.schema import Error
+from sqlite3 import DatabaseError
 import cx_Oracle
 from email import message
 
 class Baza:
     def __init__(self,c) :
         self.connection = cx_Oracle.connect(c)
-        print(c)
     def do_query(self,query):
         cur=self.connection.cursor()
         try:
             cur.execute(query)
             self.connection.commit()
-        except Exception:
-            raise Error("Nije moguce upisati podatke u bazu")
+        except cx_Oracle.DatabaseError:
+            raise cx_Oracle.DatabaseError("Database")
+    def do_query_with_result(self,query):
+        cur=self.connection.cursor()
+        try:
+            cur.execute(query)
+            result=cur.fetchall()
+            return result
+        except cx_Oracle.DatabaseError:
+            raise cx_Oracle.DatabaseError("Database")
