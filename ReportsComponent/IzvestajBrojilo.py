5be0b04da56db011d284ddcc1432fe750c98d016
OgnjenDacevic2000 <ognjendacevic2000@gmail.com>
2022-06-19 23:50:35 +0200

    Refaktorizacija koda

--- ReportsComponent/IzvestajBrojilo.py
+++
@@ -1,6 +0,0 @@
-from KonekcijaKaBazi import *
-
-sqlquery = "select * from brojilo"
-cursor = con.cursor()
-cursor.execute(sqlquery)
-rows = cursor.fetchall()
--- ReportsComponent/KonekcijaKaBazi.py
+++
@@ -1,6 +0,0 @@
-import logging
-import cx_Oracle
-
-con = cx_Oracle.connect('Korisnik/ftn@localhost')
-
-
